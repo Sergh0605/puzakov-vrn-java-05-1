91371081b97e7a741a9dd4b1cbffe2fc254e05c6
Sergh0605 <puzakovsergei@gmail.com>
2021-10-20 00:29:04 +0300

    Done

--- src/main/java/ru/dataart/academy/java/ReverseInteger.java
+++ src/main/java/ru/dataart/academy/java/ReverseInteger.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ReverseInteger {
     /**
